834a531cd83b373b527411f69d6478c2814823e6
aramirez13 <andres.ramirez.-nd@disney.com>
2025-12-30 12:39:16 -0500

    fix: added dist to demo

--- vite.demo.config.ts
+++ vite.demo.config.ts
@@ -3,7 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   root: "demo",
+  base: "./",
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
   server: {
     port: 5174,
   },
